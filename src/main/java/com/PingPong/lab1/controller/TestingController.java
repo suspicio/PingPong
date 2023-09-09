e438272c179228de130b86358358c11377299fb4
asker <askarov.alakbar@gmail.com>
2023-09-09 19:57:30 -0400

    second service

--- src/main/java/com/PingPong/lab1/controller/TestingController.java
+++ src/main/java/com/PingPong/lab1/controller/TestingController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +15,15 @@ public class TestingController {
     @Autowired
     private PingPongService pingPongService;
 
-    @GetMapping("/ping")
+    @GetMapping("/start")
     public ResponseEntity<String> startTesting() {
-        return new ResponseEntity<>("hey", HttpStatus.OK);
+        pingPongService.startPing();
+
+        return new ResponseEntity<>("started", HttpStatus.OK);
+    }
+
+    @PostMapping("/pong")
+    public ResponseEntity<String> ping(@RequestBody String data) {
+        return new ResponseEntity<>(data, HttpStatus.OK);
     }
 }
--- src/main/java/com/PingPong/lab1/utils/SingletonInstance.java
+++ src/main/java/com/PingPong/lab1/utils/SingletonInstance.java
@@ -1,5 +1,8 @@
 package com.PingPong.lab1.utils;
 
-public class SingletonInstance {
+import java.time.Duration;
+import java.util.ArrayList;
 
+public class SingletonInstance {
+    public static ArrayList<Duration> timeSpans = new ArrayList<>();
 }
