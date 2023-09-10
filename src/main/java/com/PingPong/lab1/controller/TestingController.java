b69952f205d1e65856bc66ff4c96b1b62b609a3d
asker <askarov.alakbar@gmail.com>
2023-09-09 23:06:07 -0400

    changed to self service

--- src/main/java/com/PingPong/lab1/controller/TestingController.java
+++ src/main/java/com/PingPong/lab1/controller/TestingController.java
@@ -1,27 +1,13 @@
 package com.PingPong.lab1.controller;
 
-import com.PingPong.lab1.service.PingPongService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestingController {
-
-    @Autowired
-    private PingPongService pingPongService;
-
-    @GetMapping("/start")
-    public ResponseEntity<String> startTesting() {
-        pingPongService.startPing();
-
-        return new ResponseEntity<>("started", HttpStatus.OK);
-    }
-
     @PostMapping("/pong")
     public ResponseEntity<String> ping(@RequestBody String data) {
         return new ResponseEntity<>(data, HttpStatus.OK);
--- src/main/java/com/PingPong/lab1/utils/SingletonInstance.java
+++
@@ -1,8 +0,0 @@
-package com.PingPong.lab1.utils;
-
-import java.time.Duration;
-import java.util.ArrayList;
-
-public class SingletonInstance {
-    public static ArrayList<Duration> timeSpans = new ArrayList<>();
-}
